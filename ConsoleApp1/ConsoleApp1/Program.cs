2bd6b5905982826a96a2bf09815e2a61397f75e3
Elkhan <elkhan.safarov10@yandex.ru>
2019-01-23 17:51:46 +0500

    task 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b = 0;
+            int a;
+            while (true)
+            {
+                while (!(int.TryParse(Console.ReadLine(), out a)))
+                {
+                    Console.WriteLine("Веедите число");
+                }
+                if (a != 0)
+                {
+                    if (a % 2 == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        b += a;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine($"сумму всех нечетных положительных чисел {b}");
+        }
+    }
+}
