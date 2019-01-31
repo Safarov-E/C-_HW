efbe6b568684098221acd572b7cfebb7f84d2906
Elkhan <elkhan.safarov10@yandex.ru>
2019-01-31 16:40:13 +0500

    Guess the number

---
+++ Guess the number/ConsoleApp9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = 1;
+            int max = 100;
+            int maxCount = (int)Math.Log(max - min + 1, 2) + 1;
+            int count = 0;
+            Random md = new Random();
+            int guessNumber = md.Next(min, max);
+            Console.WriteLine($"Компьютер загадал число от {min} до {max}. Попробуйте угадать его за {maxCount} попыток");
+            int n;
+            do
+            {
+                count++;
+                Console.WriteLine($"{count} попытка. Введите число:");
+                n = int.Parse(Console.ReadLine());
+                if (n > guessNumber) Console.WriteLine("Перелет!");
+                if (n < guessNumber) Console.WriteLine("Недолет!");
+            }
+            while(count < maxCount && n!=guessNumber);
+            if (n == guessNumber) Console.WriteLine($"Поздравляю! Вы угадали число за {count} попыток");
+            else Console.WriteLine("Неудача. Попробуйте еще раз");
+        }
+    }
+}
